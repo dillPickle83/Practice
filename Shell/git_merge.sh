15d526b171a498b7fb8a09e38002642de953f51e
barnacles <sp503488@gmail.com>
2024-04-29 07:17:12 -0500

    chnged cd pos

--- Shell/git_merge.sh
+++ Shell/git_merge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+cd $PRAC_DIR
 curr_branch=$(git rev-parse --abbrev-ref HEAD)
 
 echo "Current branch: $curr_branch"
-cd $PRAC_DIR
 git checkout main
 git pull origin main
 git checkout $curr_branch
