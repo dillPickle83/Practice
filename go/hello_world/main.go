996a8c2fed30fcbefad4baee2c72ac196d17a2fc
barnacles <sp503488@gmail.com>
2024-08-25 03:05:59 -0500

    consntants and variables

---
+++ go/hello_world/main.go
@@ -0,0 +1,9 @@
+package main
+// The compiler looks for this main package as the starting point to where it should start the executable from
+// The package indicates the main funcution that's defined below
+
+import "fmt"
+
+func main(){
+    fmt.Println("Hello World!")
+}
