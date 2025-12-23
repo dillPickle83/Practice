79683a2b1700a26008600d6e08c4d59759179a59
barnacles <sp503488@gmail.com>
2025-12-23 09:47:52 -0600

    need to debug the print characters changes

--- c/file_copy.c
+++ c/file_copy.c
@@ -3,6 +3,7 @@
 int main()
 {
 	int c;
+	double nc;
 
 	/*
 	c = getchar();
@@ -16,6 +17,12 @@ int main()
 	// The != operator takes precedence over the = operator and so the "c=getchar()" needs to be prioritized inside the paranthases
 	// If it were "c=getchar() != EOF", getchar() != EOF would've been evaluated first before c were assigned with the value from getchar()
 	// printf("%d", getchar() != EOF); => This expression prints 1, when the input is EOF
-	while ((c = getchar()) != EOF)
-		putchar(c);
+	//while ((c = getchar()) != EOF)
+	//	putchar(c);
+
+	printf("\nPrinting the number of characters in the string\n");
+	// Can write the above while loop in a for loop like below
+	for (nc = 0; getchar() != EOF; ++nc)
+		;
+	printf("%.0f", nc);
 }
