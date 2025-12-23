aeac35421cc0143989592c43890190ac50156715
barnacles <sp503488@gmail.com>
2025-12-23 07:19:14 -0600

    added cels to fahr conversion with for loop

--- c/temp.c
+++ c/temp.c
@@ -2,14 +2,15 @@
 
 /* Print the conversion of fahrenheit to celsius from 0-300 */
 
+# define lower 0
+# define upper 300
+# define step 20
+
+/* Can define the variables like this at the top of the code to make the code changes later more accessible, instead of initializing in the function and then setting a value */
+
 void main()
 {
 		float fahr, cels;
-		int upper, lower, step;
-
-		lower = 0;
-		upper = 300;
-		step = 20;
 
 		printf("fahrenheit\tcelcius\n");
 		fahr = lower;
@@ -22,5 +23,14 @@ void main()
 				printf("   %3.0f    \t%6.1f\n", fahr, cels);
 				fahr = fahr + step;
 		}
+
+		// Print the same but for celsius
+		fahr = lower;
+		printf("\n\ncelsius \tfahrenheit\n");
+		for (cels=upper; cels>=lower; cels=cels-step)
+		{
+       			fahr = ((9.0/5.0) * cels) + 32;
+			printf("%6.0f\t\t    %3.0f\n", cels, fahr);
+       		}
 }
 
